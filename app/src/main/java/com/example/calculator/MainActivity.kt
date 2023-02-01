7a4738beb4c1f543f1a437a74e917a7b32ba7451
Jinhyeok Park <86557453+zinhyeok@users.noreply.github.com>
2023-02-01 10:47:21 +0900

    Fix:[fun isOperatorAdded return change]

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -37,20 +37,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onOperator(view: View){
-        tvInput?.text?.let {
+        tvInput?.text?.let{
             if(lastNumeric && !isOperatorAdded(it.toString())){
                 tvInput?.append((view as Button).text)
                 lastNumeric = false
                 lastDot = false
             }
         }
+
     }
-    private fun isOperatorAdded(value: String):Boolean {
+    private fun isOperatorAdded(value: String): Boolean{
         return if(value.startsWith("-")){
             false
         } else{
             value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
-            true
         }
     }
 }
