cba321df760644549c8826a9ee60120be353840b
Jinhyeok Park <86557453+zinhyeok@users.noreply.github.com>
2023-02-01 10:34:18 +0900

    FEAT:[97 add onOperator fun]

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private var tvInput:TextView? = null
+    private var lastNumeric = false
+    private var lastDot = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,10 +19,38 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View){
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
     }
 
     fun onClear(view: View){
         tvInput?.text = ""
     }
 
+    fun onDecimalPoint(view: View){
+
+        if(lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View){
+        tvInput?.text?.let {
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+    private fun isOperatorAdded(value: String):Boolean {
+        return if(value.startsWith("-")){
+            false
+        } else{
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+            true
+        }
+    }
 }
